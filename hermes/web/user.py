a3073dfad3d8848d8d51a6e94c2d6830cbc389a0
cenenh <gseunwo@gmail.com>
2016-07-13 01:44:27 +0900

    add simple test api with db

--- hermes/web/user.py
+++ hermes/web/user.py
@@ -2,15 +2,34 @@ from aiohttp import web, Response
 from hermes.mysql import MySQL
 import asyncio
 import logging
+import json
+
+
+@asyncio.coroutine
+def add_user_handler(request):
+    data = yield from request.json()
+    try:
+        name = data['name']
+    except KeyError as e:
+        logging.error(e)
+        response = {'code': 400, 'message': "'name' is omitted"}
+    else:
+        mysql = MySQL()
+        yield from mysql.connect()
+        query = """INSERT INTO test_user(name) VALUES('%s')""" % name
+        yield from mysql.execute_query(query)
+        yield from mysql.close()
+        response = {'code': 200, 'message': 'ok'}
+    finally:
+        return web.Response(headers={'content-type': 'application/json'},
+                            text=json.dumps(response))
 
 
 @asyncio.coroutine
 def get_user_handler(request):
-    loop = asyncio.get_event_loop()
-    user_name = request.match_info.get('name', 'admin')
-    db = MySQL(loop)
-    yield from db.connect()
-    results = yield from db.execute_query('select * from test')
-    yield from db.close()
-    logging.info(results)
-    return web.Response(text='hi')
+    mysql = MySQL()
+    yield from mysql.connect()
+    results = yield from mysql.execute_query('select * from test_user')
+    yield from mysql.close()
+    return web.Response(headers={'content-type': 'application/json'},
+                        text=json.dumps(results))
